241e14361055bb0cf04650bb203d02780c51feb1
AnchalAgrahari <anchalagrahari258@gmail.com>
2024-12-25 18:12:45 +0530

    Added some basics

--- datastructure/array/easy-problems/traverse-array.py
+++ datastructure/array/easy-problems/traverse-array.py
@@ -1,21 +1,25 @@
 #creat an array havingg number 1 to 5 and then print it by using for-loop 
 
 def main():
+ 
+ # Create an array with numbers 1 to 5
+ numbers = [1, 2, 3, 4, 5]
+
+# Print each number using a for-loop
+ for number in numbers:
+    print(number)
+
+main()
 
 
+#Python program show input and output
+
+xse = input("Enter your Value: ")
+print(xse)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-	main()
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not b)
