ab528a8b510b7938452eae58bef17d66708f0518
Anchal Agrahari <anchalagrahari258@gmail.com>
2025-01-21 23:51:21 +0530

    new update

--- datastructure/array/easy-problems/traverse-array.py
+++ datastructure/array/easy-problems/traverse-array.py
@@ -4,9 +4,8 @@
 
 ### code to print the array in correct formate 
 
-def main():
     
-    def reverseArray (arr):
+def reverseArray (arr):
         left = 0                                             #initialize left to the beginning & right to the end
         right = len(arr)-1          
 
@@ -14,16 +13,21 @@ def main():
             arr[left], arr[right] = arr[right], arr[left]      #swap the  element at left to right
             left += 1                                         #increment the left pointer
             right -= 1                                        # decrement the right pointer
+
             if __name__ == "__main__":
-                arr = [1,5,4,6,3,2]
-                reverseArray(arr)
+                arr = [1,5,4,6,3,2]                            #sample array
+
+                reverseArray(arr)                              #call the function to reverse the array
+
+                # print the reversed array in correct format
                 for i in range(len(arr)):
-                    print (arr[i], end="")
+                    print(arr[i], end=" ")
 
+
+
+
+                    print ("Hello there !!!")
                     
-
-
-
 
 
 
