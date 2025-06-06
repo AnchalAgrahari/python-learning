3e39d8dacce756618fe78d0a294c6dd0c894f6e9
Anchal Agrahari <anchalagrahari258@gmail.com>
2025-06-07 01:22:36 +0530

    Adding tag in s3 bucket using locally stored JSON file : WIP

--- aws-resouce-tag/awsResourceTag.py
+++ aws-resouce-tag/awsResourceTag.py
@@ -1,7 +1,17 @@
 import json
 import boto3
-try:
 
+#reading  json file present in system
+def read_file(file_name):
+    try:
+        with open(file_name ,'r') as f :
+            data = json.load(f)
+        print(data)
+    except Exception as e:
+        print("AN exception has occured", e)
+
+#reading json file present in s3 bucket
+try:
     s3_obj = boto3.client('s3')
 
     s3_clientobj = s3_obj.get_object(Bucket = 'read-json-tagger-file-1062025', Key = 'tag.json')
@@ -16,24 +26,40 @@ try:
     print(s3clientlist)
     print(type(s3clientlist))
 except Exception as e:
-    print("An error occured", e)
+    print("An error occured while reading file ",e)
 
-def read_file(file_name):
+#adding tag
+
+def add_tag(bucket_name):
     try:
-        with open(file_name ,'r') as f :
-            data = json.load(f)
-        print(data)
+        client = boto3.client('s3')
+        response = client.put_bucket_tagging(
+            Bucket = bucket_name , 
+            ChecksumAlgorithm = 'CRC32',
+            Tagging={
+                'TagSet':  s3clientlist[ 's3clientlist']
+            },
+            ExpectedBucketOwner='697429983773'
+        )
     except Exception as e:
-        print("AN exception has occured", e)
+        print("An exception has occured during tagging",e)
 
-    
+def lambda_handler(event, context):
+    try:
+         # s3
+        bucket_name = 'read-json-tagger-file-1062025'
+        #add tag
+        add_tag(bucket_name)
+    except Exception as e:
+        print("An exception has occured")
+
 
 if __name__ == '__main__':
     try:
-        file_name = 'tag.json'
+        file_name = "tag.json"
+        #file reading
         read_file(file_name)
-
+       
+       
     except Exception as e:
         print("AN exception has occured", e)
-
-    
