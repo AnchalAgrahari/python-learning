2edccea7ed42e3af8b98e55c34d34700ea953e7a
Anchal Agrahari <anchalagrahari258@gmail.com>
2026-04-03 23:33:32 +0530

    logging configuration with FileHandler and formatter

--- logger1.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w",format="%(asctime)s - %(levelname)s - %(message)s")
-
-logging.debug("DEBUG")
-logging.info("INFO")
-logging.warning("warning")
-logging.error("error")
-logging.critical("critical")
--- logger2.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.debug, filename="log.log", filemode="w",
-                    format="%(asctime)s - %(levelname)s - %(message)s")
-
-
-x = 2
-
-logging.debug(f"the value of x  is {x}")
